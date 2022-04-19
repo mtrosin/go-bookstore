068703cb2d6ea714afd6dcb340cd87eb7e1124b0
mtrosin <mtrosin@gmail.com>
2022-04-18 23:03:26 -0300

    added code to main function

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/mtrosin/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
