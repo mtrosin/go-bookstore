05c859b08364378096959ff42dbcbb7625c1d887
mtrosin <mtrosin@gmail.com>
2022-04-26 00:31:55 -0300

    added godotenv, env variables example and changed hard-coded string to use .env data

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,6 +1,8 @@
 package config
 
 import(
+	"os"
+	"github.com/joho/godotenv"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -9,8 +11,20 @@ var (
 	db * gorm.DB
 )
 
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+  
+	if err != nil {
+	  panic("Error loading .env file")
+	}
+  
+	return os.Getenv(key)
+  }
+
 func Connect() {
-	d, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local")
+	connStr := getEnvVariable("DB_USER")+":"+getEnvVariable("DB_PASSWORD")+"@tcp("+getEnvVariable("DB_HOST")+":"+getEnvVariable("DB_PORT")+")/"+getEnvVariable("DB_NAME")+"?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open("mysql", connStr)
+
 	if err != nil {
 		panic(err)
 	}
